f0dcfa40e57cef552bb5203d87e6d937d18da7e8
SrutiranjanPraharaj <SrutiranjanPraharaj@host.docker.internal>
2020-05-15 13:50:15 +0530

    initial commit

---
+++ src/main/java/com/ranjan/springboot/SpringbootDockerCicdAwsApplication.java
@@ -0,0 +1,13 @@
+package com.ranjan.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerCicdAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerCicdAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranjan/springboot/SpringbootDockerCicdAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjan.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerCicdAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
