b41716661740b888e3eb6d42591ecc9cd074aa75
SrutiranjanPraharaj <SrutiranjanPraharaj@host.docker.internal>
2020-05-15 13:55:21 +0530

    -- Creating hello controller
    -- Adding Dockerfile & Buildspec.yml

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:8-jdk-alpine
+
+ARG JAR_FILE=target/microservice-0.1.jar
+
+WORKDIR /app
+
+COPY ${JAR_FILE} app.jar
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/ranjan/springboot/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.ranjan.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+	@GetMapping
+	public String testResponse() {
+		
+		return "Hello World!";
+	}
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String getRequest() {
+		
+		return "Welcome to Spring boot !!";
+	}
+}
